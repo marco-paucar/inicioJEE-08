9a293c976c6aba581a53e08060854ef6da7ea101
unknown <msp.paucar@gmail.com>
2022-04-03 21:13:09 -0500

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+# inicioJEE-08
+PARA CONFIGURAR y QUE todo este funcionando.
+
+para ejecutar se requiere solo RUN.
+y aparece en el navedaor ejecutandoce con el brucer por defecto
---
+++ src/main/java/com/empresa/inicioJEE_lec_08/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.empresa.inicioJEE_lec_08;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
